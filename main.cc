bac51ee1678617f2d5ff9fbe10d59edf41bf9a0e
Jordan Charest <jordancharest3@gmail.com>
2018-10-02 18:08:40 -0400

    Hello world

---
+++ main.cc
@@ -0,0 +1,19 @@
+#include <cstdlib>
+#include <iostream>
+
+#include "des_cipher.h"
+#include "udp_server.h"
+
+
+int main(int argc, char** argv) {
+  std::cout << "Hello world\n";
+
+  DES::Cipher cipher;
+  
+    
+
+
+
+
+    return EXIT_SUCCESS;
+}
